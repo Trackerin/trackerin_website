3248b3e38201e06539e4148f4472ccd5d081ea87
Vinneth <anandakevinputra208@gmail.com>
2026-06-06 15:04:55 +0700

    fix: vite config add logo svg

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,11 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                'resources/css/app.css',
+                'resources/js/app.js',
+                'resources/images/trackerin_logo.svg',
+            ],
             refresh: true,
         }),
         tailwindcss(),
